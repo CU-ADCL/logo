c0f2056903b03ff55c9f620a72aabfc69da2c064
Zachary Sunberg <zachary.sunberg@colorado.edu>
2021-12-28 10:43:37 -0700

    added square version

--- make_pngs.sh
+++ make_pngs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in ADCL ADCL-Gold ADCL-Gold-Gray ADCL-text
+for file in ADCL ADCL-Gold ADCL-Gold-Gray ADCL-text ADCL-square
 do
     inkscape -z -f ${file}.svg -w 1080 -e ${file}.png
 done
